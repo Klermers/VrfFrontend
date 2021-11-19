179a910729fb9b86ddca02112415f09853d045aa
klerm <jeremeymain@gmail.com>
2021-11-19 07:42:24 +0100

    didnt get commited?

--- src/components/Events/Events.vue
+++ src/components/Events/Events.vue
@@ -1,15 +1,15 @@
 <template>
-<div/>
+<v-container style="margin-left: 250px;">
+<forum-table/>
+</v-container>
 </template>
 
 <script>
+import ForumTable from './ForumTable.vue';
 export default {
+  components: { ForumTable },
   name: 'Events',
-  components: {
-  },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
+<style scoped>
+</style>
